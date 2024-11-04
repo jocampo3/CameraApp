24e417dc83db8df298747c5afe6c688f03e72565
Jose Ocampo <ocampojose305@gmail.com>
2024-11-04 10:50:37 -0600

    pushing working app to main

--- src/main/java/com/devicemanage/CameraCapture.java
+++ src/main/java/com/devicemanage/CameraCapture.java
@@ -1,27 +1,78 @@
 package com.devicemanage;
 
-import com.github.sarxos.webcam.Webcam;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
+
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import com.github.sarxos.webcam.Webcam;
 
 public class CameraCapture {
 
-    public static void main(String[] args) throws IOException {
-        // Get the default webcam
-        Webcam webcam = Webcam.getDefault();
+    public static void main(String[] args) {
 
-        // Open the webcam
-        webcam.open();
+        // Create new JFrame to contain both
+        // the "Capture Image" button and 
+        // the captured image.
+        JFrame frame = new JFrame("Camera Capture");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
 
-        // Capture an image
-        BufferedImage image = webcam.getImage();
+        // Create new JPanel w/ a button.
+        JPanel panel = new JPanel(new BorderLayout());
 
-        // Save the image to a file
-        ImageIO.write(image, "PNG", new File("captured_image.png"));
+        JButton captureButton = new JButton("Capture Image");
+        panel.add(captureButton, BorderLayout.NORTH);
 
-        // Close the webcam
-        webcam.close();
+        JLabel imageLabel = new JLabel();
+        panel.add(imageLabel, BorderLayout.CENTER);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+        // Message dialog that tells the user
+        // that their image has been captured.
+        captureButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Image Captured!");
+            }
+        });
+        
+        // Add camera capture feature 
+        // to the button.
+        captureButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Webcam webcam = Webcam.getDefault(); // Get default webcam.
+                    if (webcam != null) {
+                        webcam.open(); // Open webcam.
+                        BufferedImage image = webcam.getImage(); // Capture image.
+                        webcam.close(); // Close webcam.
+
+                        // Save captured image.
+                        ImageIO.write(image, "PNG", new File("captured_image.png"));
+
+                        imageLabel.setIcon(new javax.swing.ImageIcon(image));
+
+                    } else {
+                        // Message dialog if no webcam
+                        // is detected.
+                        System.out.println("No webcam detected");
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
     }
 }
